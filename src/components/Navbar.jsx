68483faeb849c7d8b7610030272e1eb7d861ecee
unknown <doni.yukri@gmail.com>
2022-01-21 13:58:11 +0700

    minor changes on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -30,7 +30,7 @@ function Navbar() {
                   : "navbarListItemName"
               }
             >
-              Explore
+              Jelajahi
             </p>
           </li>
           <li className="navbarListItem" onClick={() => navigate("/offers")}>
@@ -46,7 +46,7 @@ function Navbar() {
                   : "navbarListItemName"
               }
             >
-              Offers
+              Diskon
             </p>
           </li>
           <li className="navbarListItem" onClick={() => navigate("/profile")}>
@@ -62,7 +62,7 @@ function Navbar() {
                   : "navbarListItemName"
               }
             >
-              Profile
+              Profil
             </p>
           </li>
         </ul>
